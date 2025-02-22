13da2dedef51b058a8eb6f654738f9da389afcc6
Israel Alves <alves.israel@aluno.ifce.edu.br>
2025-02-22 18:10:30 -0300

    Update wine.py

--- src/wine.py
+++ src/wine.py
@@ -33,7 +33,7 @@ X_train_scaled = scaler.fit_transform(X_train)
 data = pd.DataFrame(data=X, columns=wine.feature_names)
 data["target"] = wine.target
 data["target name"] = pd.Categorical.from_codes(y, wine.feature_names)
-data
+# data #use se estiver usando em um notebook jupyter ou colab
 
 #treinando o SVM  e exibindo os resultados
 
